b3e2b2241889318a043f755f8ef5e456288b1e4b
Ballabh Heera <44017701+heerajoshi@users.noreply.github.com>
2019-01-18 11:26:58 +0530

    now its working show the comments or
    save the comments in anthor file
    we have to decode the comments now

--- src/app.js
+++ src/app.js
@@ -1,24 +1,87 @@
+
 const fs = require("fs");
+const comments = require("./comments.json");
+const Handler = require("./handler.js");
+const app = new Handler();
 
-const getFile = function(url) {
-  if (url === "/") return "./public/index.html";
-  return "./public" + url;
-};
-
-const app = (req, res) => {
-  let path = getFile(req.url);
-  fs.readFile(path, function(err, data) {
-    try {
-      res.statusCode = 200;
-      res.write(data);
-      res.end();
-    } catch (error) {
-      res.statusCode = 404;
-      res.end();
-    }
+const readBody = (req, res, next) => {
+  let content = "";
+  req.on("data", chunk => (content += chunk));
+  req.on("end", () => {
+    req.body = content;
+    next();
   });
 };
 
-// Export a function that can act as a handler
+const send = (res, content, statusCode = 200) => {
+  res.statusCode = statusCode;
+  res.write(content);
+  res.end();
+};
 
-module.exports = app;
+const getURLPath = function(url) {
+  if (url == "/") return "./public/index.html";
+  return "./Public" + url;
+};
+
+const readContent = function(res, url) {
+  const path = getURLPath(url);
+  fs.readFile(path, (err, content) => {
+    if (err) {
+      send(res, "Invalid_Request", 404);
+      return;
+    }
+    send(res, content);
+  });
+};
+
+const renderURL = (req, res) => {
+  let url = req.url;
+  readContent(res, url);
+};
+
+const readArgs = text => {
+  let args = {};
+  text
+    .split("&")
+    .map(element=> element.split("="))
+    .forEach(([key,value])=>args[key]=value);
+  return args;
+};
+
+const getMessage = (res, messageData) => {
+  return `<h2>${messageData.name}<h2><h3>${messageData.date}</h3><p>${
+    messageData.comment
+  }</p>`;
+};
+
+const writeComments = function(res) {
+  fs.readFile('./public/guest.html', (err, content)=>{
+    comments.forEach(data => {
+      let message = getMessage(res, data);
+      content += message;
+    });
+    send(res, content)
+  })
+};
+
+const renderGuestBook = (req, res) => {
+  const text = req.body;
+  let { name, comment } = readArgs(text);
+  let date = new Date();
+  comments.unshift({ name, comment, date: date.toLocaleString() });
+  fs.writeFile('./src/comments.json', JSON.stringify(comments), (err) => {
+    return;
+  });
+  writeComments(res);
+};
+
+const renderError = (req, res, err) => {
+  send(res);
+};
+
+app.use(readBody);
+app.get(renderURL);
+app.post("/guest.html", renderGuestBook);
+app.error(renderError);
+module.exports = app.handleRequest.bind(app);
